c745a39f1d70ea704cec7a2186ab796ccf31c7fd
tcskill <tskill@us.ibm.com>
2023-06-30 16:45:48 -0500

    adds product searching

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
+
 using Core.Entities;
 
 namespace Core.Specifications
@@ -10,7 +6,8 @@ namespace Core.Specifications
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
         public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
-            : base(x => 
+            : base(x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) && 
                 (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
                 (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
