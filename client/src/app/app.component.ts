d0c2027ebd04af98b10021874bbc7ea23a918233
Tom Skill <tcskill@yahoo.com>
2023-07-05 10:24:26 -0500

    add product api call

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: any[] = []; //initial empty value
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response: any) => this.products = response.data,
+      error: error =>  console.log(error), // what to do if error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra stuff');
+      }
+    })
+  }
 }
